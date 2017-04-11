97a91ba29c27558456ebcffa47914fce58129722
CandicePersil <candice.persil@gmail.com>
2017-04-11 18:26:27 +0300

    add function.h

---
+++ src/functions.h
@@ -0,0 +1,20 @@
+/*************************************************************
+* author: Candice PERSIL                                     *
+* version: 1                                                 *	
+*						function.h 							 *
+* 				Prototypes of function.c					 *
+*															 *
+**************************************************************/
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+extern float sample_rate = 44.1f;
+extern float decay = 0.5f;
+extern double buffer[];
+
+void delay_line(double *buffer);
+
+
+
+#endif // FUNCTIONS_H
